79b5ca5053aba93cf5a7a5f1c388a56468cd73d5
Quentin <quentin.beaudiquez@gmail.com>
2025-01-09 20:49:12 +0100

    init mongodb docker

--- backend/src/config.php
+++ backend/src/config.php
@@ -6,7 +6,28 @@ $password = getenv("MYSQL_PASSWORD");
 $dbname = getenv("MYSQL_DATA_BASE");
 
 try {
-    $connexion = new PDO("mysql:host=$serveurname;dbname=$dbname", $username, $password);
+    $connexion = new PDO(
+        "mysql:host=$serveurname;
+        port=3306;
+        dbname=$dbname", 
+        $username, 
+        $password);
 } catch (PDOException $e) {
     echo "Erreur : " . $e->getMessage();
 }
+
+$mongoHost = 'mongo';
+$mongoPort = '27017';
+$mongoDbName = getenv("MONGO_INITDB_ROOT_USERNAME");
+$mongoPassword = getenv("MONGO_INITDB_ROOT_PASSWORD");
+$mongoUser = getenv("MONGO_INITDB_ROOT_USERNAME");
+
+try {
+    $manager = new MongoDB\Driver\Manager("mongodb://$mongoUser:$mongoPassword@$mongoHost:$mongoPort");
+
+} catch (MongoDB\Driver\Exception\Exception $e) {
+    echo "Erreur de connexion à MongoDB : " . $e->getMessage();
+}
+
+
+?>
